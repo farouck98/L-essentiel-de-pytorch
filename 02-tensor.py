b58eb72307f2342c6b1057c612d9732556165d4e
farouck98 <latoundjifarouck@gmail.com>
2025-02-10 07:05:56 +0100

    créer des tensors

---
+++ 02-tensor.py
@@ -0,0 +1,13 @@
+import torch
+
+a1 = torch.empty(1)
+print(f"a1={a1}")
+
+a2 = torch.empty(2)
+print(f"a2={a2}")
+
+a3 = torch.empty(3)
+print(f"a3={a3}")
+
+a4 = torch.empty(2,3)
+print(f"a4={a4}")
