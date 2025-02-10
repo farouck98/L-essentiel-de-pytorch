6a563bac7cf217ab659473b46b5b4c4be5f28645
farouck98 <latoundjifarouck@gmail.com>
2025-02-10 07:17:50 +0100

    Utiliser des tensors

---
+++ 03-tensor.py
@@ -0,0 +1,13 @@
+import torch
+
+#tensor généré avec des valeurs aléatoires
+a5 = torch.rand(2,2)
+print(f"a5={a5}")
+
+#tensor à 3 valeurs initialisés avec les valeurs zéros
+a6 = torch.zeros(3)
+print(f"a6={a6}")
+
+#tensor à 3 valeurs initialisés avec les valeurs UN
+a7 = torch.ones(3)
+print(f"a7={a7}")
